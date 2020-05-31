d5f3dd2ca4dd3d4f6bb5ba27df3e4fcd19af91ed
Leonicksis <58902899+Leonicksis@users.noreply.github.com>
2020-05-31 18:41:29 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        ModelT me_phone = new ModelT();
+        me_phone.Info();
+        me_phone.StartScreen();
+
+    }
+}
---
+++ src/com/company/ModelT.java
@@ -0,0 +1,12 @@
+
+package com.company;
+
+public class ModelT extends SiemensMob{
+
+    private String model_name = "SUR20PLZ11";
+    @Override
+    public void Info() {
+        System.out.println(company + " serial:" + model_name);
+        System.out.println("Made in China, bro :C");
+    }
+}
